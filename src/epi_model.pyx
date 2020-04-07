784a41eba48d676f6c4610b595e2b4d5ae5ba6bf
Alexey Vlasenko <alexey.vlasenko@gmail.com>
2020-04-06 20:04:01 -0400

    input and output wrappers

--- src/epi_model.pyx
+++ src/epi_model.pyx
@@ -6,6 +6,9 @@
 # distutils: sources = ./src/epi_lib/single_source.c
 # distutils: include_dirs = ./epi_lib/
 
+cdef extern from "stdbool.h":
+    ctypedef bint bool
+
 cimport cepi_model
 
 def HandleError(cepi_model.EpiError err):
@@ -16,10 +19,56 @@ def HandleError(cepi_model.EpiError err):
     # TODO: add remaining error handling cases
     raise ValueError()
 
+class EpiScenario:
+    t_initial = 0
+    n_initial = 10
+    t_vaccine = 550
+    t_max = -1
+    dis_fname = "./dat/disease.dat"
+    pop_fname = "./dat/population.dat"
+
+class EpiInput:
+    dist_recommend = False
+    dist_home_symp = False
+    dist_home_all = False
+
+class EpiObservables:
+    day = 0
+    finished = False
+    vaccine_available = False
+    hosp_capacity = 0
+    n_susceptible = 0
+    n_infected = 0
+    n_critical = 0
+    n_recovered = 0
+    n_vaccinated = 0
+    n_dead = 0
+
+    def __cinit__(self, cepi_model.EpiObservable obs):
+        self.day = obs.day
+        self.finished = obs.finished
+        self.vaccine_available = obs.vaccine_available
+        self.hosp_capacity = obs.hosp_capacity
+        self.n_susceptible = obs.n_susceptible
+        self.n_infected = obs.n_infected
+        self.n_critical = obs.n_critical
+        self.n_recovered = obs.n_recovered
+        self.n_vaccinated = obs.n_vaccinated
+        self.n_dead = obs.n_dead
+
 cdef class EpiModel:
     cdef cepi_model.EpiModel _c_model
 
-    def __cinit__(self, cepi_model.EpiScenario sc):
+    def __cinit__(self, scenario):
+        cdef cepi_model.EpiScenario sc
+
+        sc.t_initial = scenario.t_initial
+        sc.t_initial = scenario.n_initial
+        sc.t_initial = scenario.t_vaccine
+        sc.t_initial = scenario.t_max
+        sc.t_initial = scenario.dis_fname
+        sc.t_initial = scenario.pop_fname
+
         cdef cepi_model.EpiError err
         err = cepi_model.epi_construct_model(&self._c_model, &sc)
         HandleError(err)
@@ -29,14 +78,23 @@ cdef class EpiModel:
         err = cepi_model.epi_free_model(&self._c_model)
         HandleError(err)
 
-    def step(self, cepi_model.EpiInput input):
+    def step(self, input):
+        cdef cepi_model.EpiInput inp
+
+        inp.dist_recommend = input.dist_recommend
+        inp.home_symp = input.home_symp
+        inp.home_all = input.home_all
+
         cdef cepi_model.EpiError err
-        err = cepi_model.epi_model_step(self._c_model, &input)
+        err = cepi_model.epi_model_step(self._c_model, &inp)
         HandleError(err)
 
-    def get_observable(self):
+    def get_observables(self):
         cdef cepi_model.EpiError err
         cdef cepi_model.EpiObservable output
         err = cepi_model.epi_get_observables(&output, self._c_model)
         HandleError(err)
-        return output
+
+        out = EpiObservables(output)
+
+        return out

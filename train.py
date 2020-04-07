03e799d0ddead70126cc79ef18cf35718fdd60bd
Alexey Vlasenko <alexey.vlasenko@gmail.com>
2020-04-06 22:26:54 -0400

    imports

---
+++ train.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, InputLayer
